62ffb4d88ffbcb33d6240b8149e8048352030a5e
nguyenhoan1209 <nguyenvanhoan0902@gmail.com>
2024-05-05 15:51:02 +0700

    feature: set active page in sidebar

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,11 +1,21 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import { Home, CreditCard, StickyNote } from "lucide-react";
+import { useLocation } from "react-router-dom";
+import { Home, CreditCard, StickyNote, LogIn, BookUser } from "lucide-react";
 
 const SidebarMenu = () => {
   const [token, setToken] = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const location = useLocation();
+  const [activePage, setActivePage] = useState("");
+
+  useEffect(() => {
+    // Update activePage state based on URL pathname
+    const pathname = location.pathname;
+    setActivePage(pathname);
+  }, [location.pathname]);
 
   const logoutUser = () => {
     // e.preventDefault()
@@ -17,7 +27,7 @@ const SidebarMenu = () => {
   return (
     <>
       <div>
-        <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <nav className="fixed top-0 z-50 w-full bg-gray-100 border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
           <div className="px-3 py-3 lg:px-5 lg:pl-3">
             <div className="flex items-center justify-between">
               <div className="flex items-center justify-start rtl:justify-end">
@@ -26,21 +36,21 @@ const SidebarMenu = () => {
                   data-drawer-toggle="logo-sidebar"
                   aria-controls="logo-sidebar"
                   type="button"
-                  className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                  class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 >
-                  <span className="sr-only">Open sidebar</span>
+                  <span class="sr-only">Open sidebar</span>
                   <svg
-                    className="w-6 h-6"
+                    class="w-6 h-6"
                     aria-hidden="true"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      clipRule="evenodd"
-                      fillRule="evenodd"
+                      clip-rule="evenodd"
+                      fill-rule="evenodd"
                       d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                    />
+                    ></path>
                   </svg>
                 </button>
                 <a href="#" className="flex ms-2 md:me-24">
@@ -118,13 +128,13 @@ const SidebarMenu = () => {
           className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
           aria-label="Sidebar"
         >
-          <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+          <div className="h-full px-3 pb-4 overflow-y-auto bg-gray-100 ">
             <ul className="space-y-2 font-medium">
               <li>
                 <a
                   href="/"
                   target="_self"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group "
+                  className={`flex items-center p-2 rounded-lg text-black hover:scale-110 group ${activePage === "/" && "bg-gray-300"}`}
                 >
                   <Home />
                   <span className="ms-3">Home</span>
@@ -135,7 +145,7 @@ const SidebarMenu = () => {
                 <a
                   href="/ekyc"
                   target="_self"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group "
+                  className={`flex items-center p-2 rounded-lg text-black hover:scale-110 group ${activePage === "/ekyc" && "bg-gray-300"}`}
                 >
                   <CreditCard />
                   <span className="flex-1 ms-3 whitespace-nowrap">EKYC</span>
@@ -145,7 +155,7 @@ const SidebarMenu = () => {
               <li>
                 <a
                   href="#"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group "
+                  className="flex items-center p-2 text-black rounded-lg hover:scale-110 group "
                 >
                   <StickyNote />
                   <span className="flex-1 ms-3 whitespace-nowrap">Savis</span>
@@ -157,23 +167,9 @@ const SidebarMenu = () => {
                     <a
                       href="/login"
                       target="_self"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                      className="flex items-center p-2 text-black hover:scale-110 group"
                     >
-                      <svg
-                        className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 18 16"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
-                        />
-                      </svg>
+                      <LogIn />
                       <span className="flex-1 ms-3 whitespace-nowrap">
                         Sign In
                       </span>
@@ -183,19 +179,9 @@ const SidebarMenu = () => {
                     <a
                       href="/register"
                       target="_self"
-                      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                      className="flex items-center p-2 text-black  hover:scale-110 group"
                     >
-                      <svg
-                        className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
-                        <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
-                        <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
-                      </svg>
+                      <BookUser />
                       <span className="flex-1 ms-3 whitespace-nowrap">
                         Sign Up
                       </span>
